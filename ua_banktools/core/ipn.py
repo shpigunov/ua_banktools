25775ae0a4292a7b1e7c0ff15a92e087cbaa5d78
Anton Shpigunov <shpigunov@gmail.com>
2022-11-25 21:27:17 +0200

    Update ipn.py
    Update the IPN class with English docstrings and misc tweaks

--- ua_banktools/core/ipn.py
+++ ua_banktools/core/ipn.py
@@ -29,33 +29,37 @@ class IPN:
     def __str__(self) -> str:
         return self.code
 
+    def __repr__(self) -> str:
+        return f"IPN: {self.code}"
+
     @property
     def inferred_dob(self) -> dt.date:
-        """Перші п'ять цифр кодують дату народження власника номера — зазвичай,
-        це п'ятизначне число є кількістю днів від 31 грудня 1899 року до дати
-        народження особи.
+        """The first five digits encode the bearer's date of birth: usually, it's
+        the 5-digit number corresponding to the number of days elapsed from
+        December 31, 1899 to the bearer's date of birth.
 
-        Однак, якщо однакову дату народження мають більше 5000 чоловіків (або
-        5000 жінок)[8], то в деяких із них перші п'ять цифр будуть іншими.
-        Наприклад, для дати народження 1 січня 1947 року реєстраційний номер
-        може починатися як із цифри «1», так і з цифри «8»."""
+        However, if over 5,000 men or women happen to be born on a particular day,
+        some of them may have different digits in this section of the IPN. For
+        example, for January 1, 1947, the number can start with both `1` and `8`.
+        """
 
         return dt.date(1899, 12, 31) + dt.timedelta(int(self.code[0:5]))
 
     @property
     def inferred_sex(self) -> str:
-        """У передостанній цифрі закодовано стать власника:
-        * якщо цифра непарна, стать — чоловіча
-        * якщо парна — жіноча."""
+        """The digit before the last one encodes the bearer's sex:
+        * male if the digit is odd, and
+        * female if the digit is even."""
 
         return "female" if int(self.code[-2]) % 2 == 0 else "male"
 
     @staticmethod
     def validate_control_digit(code: str) -> bool:
-        """Десята цифра — контрольна."""
+        """The digit in the tenth (last) position serves as a checksum digit."""
 
-        # Алгоритм для її розрахунку такий (вважатимемо, що код має вигляд АБВГҐДЕЄЖЗ):
-        # Розраховуємо контрольну суму Х = А*(-1) + Б*5 + В*7 + Г*9 + Ґ*4 + Д*6 + Е*10 + Є*5 + Ж*7
+        # The checksum algorithm is as follows:
+        # If the code has the format `ABCDEFGHIJ`, calculate the checksum as:
+        # Х = A*(-1) + B*5 + C*7 + D*9 + E*4 + F*6 + G*10 + H*5 + I*7
         d = [int(c) for c in code]
         X = (
             d[0] * -1
@@ -69,13 +73,11 @@ class IPN:
             + d[8] * 7
         )
 
-        # Остача від ділення контрольної суми на одинадцять буде контрольним
-        # числом: Кч = Х - (11 * ціла частина від (Х/11)) (для Microsoft Excel
-        # виглядає наступним чином =X-ROUND(X/11; 0)*11) або =ОСТАТ(X;11), для
-        # англомовних версій та OpenOffice/LibreOffice Calc =MOD(X;11)).
-        checksum = X - (11 * (X // 11))
+        # The remainder from the division of the checksum by 11 will be the check
+        # number: check_number = X - (11 * (X // 11)), or:
+        check_number = X % 11
 
-        # Контрольне число може мати значення від нуля до десяти. У разі
-        # залишку 10 старша одиниця відкидається. Контрольною цифрою буде
-        # остання цифра контрольного числа (тобто, контрольне число за модулем 10):
-        return checksum % 10 == d[-1]
+        # The check number may turn out to be between 0 to 10. In case the
+        # remainder is 10, the `1` in the first position is dropped. The resulting
+        # one digit must coincide with the last digit of the IPN.
+        return check_number % 10 == d[-1]
